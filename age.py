7e9121e534340dc01415274b67f12d082958f3d6
Ved Jaggi <vedajaggi@gmail.com>
2025-02-14 10:53:40 +0800

    second set of qs
    for age, name formatting and counting spaces

---
+++ age.py
@@ -0,0 +1,1 @@
+print(f"Hello {input("What is your name: ")}, you are {input("What is your age: "}")
---
+++ nformat.py
@@ -0,0 +1,3 @@
+name = input("Name: ").lower()
+name = name[0].upper() + name[1:]
+print(f"Hello {name}")
